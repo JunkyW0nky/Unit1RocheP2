b8d192914810d079457bd012e484322e5f9cde01
JunkyW0nky <723469@student.dusd.net>
2025-03-12 09:41:52 -0700

    Completed main objective
    working on final addings

--- U1PlayerControlRoche/Assets/Scripts/PlayerController.cs
+++ U1PlayerControlRoche/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   public float speed = 5.0f;
+   private float speed = 20.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +15,17 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
 
-  
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+private float forwardInput;
+
     void Update()
     {
-        // Move the vehicle foward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * 20 * forwardInput );
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
